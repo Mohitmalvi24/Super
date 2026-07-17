76b3e84b98285dd2fad79328de6ff353b8f833a6
Mohitmalvi24 <your-email@example.com>
2026-07-17 22:01:29 +0530

    feat: deplomen foe ply

--- hobby-mastery-mobile/app.config.js
+++ hobby-mastery-mobile/app.config.js
@@ -4,6 +4,9 @@ module.exports = {
   ...appJson.expo,
   extra: {
     ...appJson.expo.extra,
+    eas: {
+      ...appJson.expo.extra?.eas,
+    },
     pexelsApiKey: process.env.EXPO_PUBLIC_PEXELS_API_KEY || '',
   },
 };
